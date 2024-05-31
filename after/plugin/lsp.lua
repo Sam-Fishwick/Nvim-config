d24a77920b7869523f4f40ada1b54f0558dd51f6
Sam-Fishwick <sam.fishwick@hotmail.com>
2024-05-31 13:28:18 +0100

    minor change to Mason config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -13,7 +13,9 @@ end)
 
 -- to learn how to use mason.nvim with lsp-zero
 -- read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
-require('mason').setup({})
+require('mason').setup({
+    PATH = 'prepend',
+})
 require('mason-lspconfig').setup({
   ensure_installed = { 'lua_ls' },
   handlers = {
